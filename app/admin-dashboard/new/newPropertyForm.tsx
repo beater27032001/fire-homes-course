7c40abeefad98b839b796ba3bf4764809e4c1407
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-08 17:31:20 -0300

    (feature): finish property form fields

--- app/admin-dashboard/new/newPropertyForm.tsx
+++ app/admin-dashboard/new/newPropertyForm.tsx
@@ -1,12 +1,19 @@
 "use client"
 import PropertyForm from "@/components/property-form"
 import { propertyDataSchema } from "@/validation/propertySchema"
+import { PlusCircleIcon } from "lucide-react"
 import { z } from "zod"
 
 export default function NewPropertyForm() {
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
-
+    console.log({ data })
   }
 
-  return <div><PropertyForm /></div>
+  return (
+    <div>
+      <PropertyForm handleSubmit={handleSubmit} submitButtonLabel={<>
+        <PlusCircleIcon /> Create Property
+      </>} />
+    </div>
+  )
 }
--- app/admin-dashboard/new/page.tsx
+++ app/admin-dashboard/new/page.tsx
@@ -3,20 +3,22 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import NewPropertyForm from "./newPropertyForm";
 
 export default function NewProperty() {
-  return <div>
-    <Breadcrumbs items={[{
-      href: "/admin-dashboard",
-      label: "Dashboard"
-    }, {
-      label: "New Property"
-    }]} />
-    <Card className="mt-5">
-      <CardHeader>
-        <CardTitle className="text-3xl font-bold">New Property</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <NewPropertyForm />
-      </CardContent>
-    </Card>
-  </div>
+  return (
+    <div>
+      <Breadcrumbs items={[{
+        href: "/admin-dashboard",
+        label: "Dashboard"
+      }, {
+        label: "New Property"
+      }]} />
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle className="text-3xl font-bold">New Property</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <NewPropertyForm />
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
