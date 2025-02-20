0202f87c0ed9eb6828cb9a123b330f607af00dcd
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-20 19:22:20 -0300

    (featuer): render properties list in a table

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,12 +1,11 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
-import { getProperties } from "@/data/properties";
 import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
+import PropertiesTable from "./properties-table";
 
 export default async function AdminDashboard() {
-  const data = await getProperties()
-  console.log(data)
+
   return <div>
     <Breadcrumbs items={[{
       label: "Dashboard"
@@ -17,5 +16,6 @@ export default async function AdminDashboard() {
         <PlusCircleIcon /> New Property
       </Link>
     </Button>
+    <PropertiesTable />
   </div >
 }
---
+++ types/property.ts
@@ -0,0 +1,12 @@
+export type Property = {
+  id: string
+  address1: string
+  address2?: string
+  city: string
+  postcode: string
+  description: string
+  price: number
+  bedrooms: number
+  bathrooms: number
+  status: "for-sale" | "draft" | "withdrawn" | "sold"
+}
