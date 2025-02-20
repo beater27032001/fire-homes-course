25835c995f8b2f731ec34db82be926d65e6c74bb
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-20 18:52:14 -0300

    (feature): add toast

--- app/admin-dashboard/new/newPropertyForm.tsx
+++ app/admin-dashboard/new/newPropertyForm.tsx
@@ -5,15 +5,39 @@ import { propertyDataSchema } from "@/validation/propertySchema"
 import { PlusCircleIcon } from "lucide-react"
 import { z } from "zod"
 import { saveNewProperty } from "./actions"
+import { useToast } from "@/hooks/use-toast"
+import { useRouter } from "next/navigation"
 
 export default function NewPropertyForm() {
   const auth = useAuth()
+  const { toast } = useToast()
+  const router = useRouter()
+
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
     const token = await auth?.currentUser?.getIdToken() // Obtém o token de autenticação
 
     if (!token) return // Se não houver token, retorna
 
     const response = await saveNewProperty({ ...data, token }) // Salva a nova propriedade
+
+    if (!!response.error) {
+      toast({
+        title: "Error!",
+        description: response.message,
+        variant: 'destructive'
+      })
+
+      return
+    }
+
+    toast({
+      title: "Success!",
+      description: "Property created",
+      variant: 'success'
+    })
+
+    router.push("/admin-dashboard")
+
     console.log({ response })
   }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from "@/context/auth";
 import AuthButtons from "@/components/auth-buttons";
 import { HomeIcon } from "lucide-react";
 import { Poppins } from "next/font/google"
+import { Toaster } from "@/components/ui/toaster";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -42,6 +43,7 @@ export default function RootLayout({
             </ul>
           </nav>
           {children}
+          <Toaster />
         </AuthProvider>
       </body>
     </html>
