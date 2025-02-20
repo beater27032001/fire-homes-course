3bf9fa76ffa5928f9d83a069e245727649698a2c
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-20 18:39:19 -0300

    (feature): create save new property

--- app/admin-dashboard/new/newPropertyForm.tsx
+++ app/admin-dashboard/new/newPropertyForm.tsx
@@ -1,12 +1,20 @@
 "use client"
 import PropertyForm from "@/components/property-form"
+import { useAuth } from "@/context/auth"
 import { propertyDataSchema } from "@/validation/propertySchema"
 import { PlusCircleIcon } from "lucide-react"
 import { z } from "zod"
+import { saveNewProperty } from "./actions"
 
 export default function NewPropertyForm() {
+  const auth = useAuth()
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
-    console.log({ data })
+    const token = await auth?.currentUser?.getIdToken() // Obtém o token de autenticação
+
+    if (!token) return // Se não houver token, retorna
+
+    const response = await saveNewProperty({ ...data, token }) // Salva a nova propriedade
+    console.log({ response })
   }
 
   return (
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,41 @@
+// Importa a função cookies do Next.js para manipulação de cookies
 import { cookies } from "next/headers";
+// Importa os tipos NextRequest e NextResponse do Next.js para manipulação de requisições e respostas
 import { NextRequest, NextResponse } from "next/server";
+// Importa a função decodeJwt da biblioteca jose para decodificação de tokens JWT
 import { decodeJwt } from 'jose'
 
+// Função middleware para manipulação de requisições
 export async function middleware(request: NextRequest) {
-  console.log("MIDDLEWARE", request.url)
+  console.log("MIDDLEWARE", request.url) // Loga a URL da requisição para debug
+
+  // Se o método da requisição for POST, permite a continuação da requisição
   if (request.method === "POST") {
     return NextResponse.next()
   }
 
+  // Obtém a instância de manipulação de cookies
   const cookieStore = await cookies()
+  // Obtém o valor do token de autenticação do cookie
   const token = cookieStore.get("firebaseAuthToken")?.value
 
+  // Se não houver token, redireciona para a página inicial
   if (!token) {
     return NextResponse.redirect(new URL("/", request.url))
   }
 
+  // Decodifica o token JWT
   const decodedToken = decodeJwt(token)
+  // Se o token decodificado não tiver a claim de admin, redireciona para a página inicial
   if (!decodedToken.admin) {
     return NextResponse.redirect(new URL("/", request.url))
   }
 
+  // Permite a continuação da requisição
   return NextResponse.next()
 }
 
+// Configuração do middleware para aplicar apenas na rota /admin-dashboard
 export const config = {
   matcher: [
     "/admin-dashboard"
