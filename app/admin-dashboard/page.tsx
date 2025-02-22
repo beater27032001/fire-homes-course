37396d212fcf4e66f44427b11957a304edf5de85
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-21 23:18:35 -0300

    (feature): pagination button

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -4,7 +4,11 @@ import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
 import PropertiesTable from "./properties-table";
 
-export default async function AdminDashboard() {
+export default async function AdminDashboard({
+  searchParams
+}: { searchParams?: Promise<any> }) {
+  const searchParamsValue = await searchParams
+  console.log({ searchParamsValue })
 
   return <div>
     <Breadcrumbs items={[{
@@ -16,6 +20,6 @@ export default async function AdminDashboard() {
         <PlusCircleIcon /> New Property
       </Link>
     </Button>
-    <PropertiesTable />
+    <PropertiesTable page={searchParamsValue?.page ? parseInt(searchParamsValue.page) : 1} />
   </div >
 }
