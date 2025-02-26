969e3078ead502b60f8604e8a4f00e5b3efb2d97
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-02-26 19:48:35 -0300

    (feature): render image list

--- components/multi-image-uploader.tsx
+++ components/multi-image-uploader.tsx
@@ -2,6 +2,10 @@
 
 import React, { useRef } from "react"
 import { Button } from "./ui/button"
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
+import Image from "next/image"
+import { Badge } from "./ui/badge"
+import { MoveIcon, XIcon } from "lucide-react"
 
 export type ImageUpload = {
   id: string
@@ -44,9 +48,50 @@ export default function MultiImageUploader({
         accept="image/*"
         onChange={handleInputChange}
       />
-      <Button onClick={() => uploadInputRef?.current?.click()} type="button">
+      <Button
+        className="w-full"
+        variant='outline'
+        onClick={() => uploadInputRef?.current?.click()}
+        type="button">
         Upload images
       </Button>
+      <DragDropContext onDragEnd={() => { }}>
+        <Droppable droppableId="property-images" direction="vertical">
+          {(provided) => (
+            <div{...provided.droppableProps} ref={provided.innerRef}>
+              {images.map((image, index) => (
+                <Draggable key={image.id} draggableId={image.id} index={index}>
+                  {(provided) => (
+                    <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="relative p-2">
+                      <div className="bg-gray-100 rounded-lg flex gap-2 items-center overflow-hidden">
+                        <div className="size-16 relative">
+                          <Image src={image.url} alt="" fill className="object-cover" />
+                        </div>
+                        <div className="flex-grow">
+                          <p className="text-sm font-medium">
+                            Image {index + 1}
+                          </p>
+                          {index === 0 && (
+                            <Badge variant='success'>
+                              Featured Image
+                            </Badge>
+                          )}
+                        </div>
+                        <div className="flex items-center p-2">
+                          <button className="text-red-500 p-2"><XIcon /></button>
+                          <div className="text-gray-500">
+                            <MoveIcon />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
